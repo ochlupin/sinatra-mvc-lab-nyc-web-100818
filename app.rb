f17bbfe999b70636647f6b443d00b156b8aa25ec
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-30 07:42:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new # new instance of PigLatinizer class
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    # calls #piglatinize method from PigLatinizer class and feeds it params[:user_phrase] as the argument
+    erb :results
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,45 @@
+class PigLatinizer
+
+  #PigLatinizer broken down into 3 parts
+   # 1) Check if a character is a consonant
+   # 2) Piglatinize a single word
+   # 3) Piglatinize a sentence
+   #  wrapped in single piglatinize method which checks if a the argument is a string or sentence and runs the appropriate method
+
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/) # checks if not = to vowel
+  end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+
+
+
+end
